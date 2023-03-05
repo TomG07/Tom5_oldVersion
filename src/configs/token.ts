d185c9ffee37f62c54e209f17b9b25ac478fab24
TomG <81299743+TomG07@users.noreply.github.com>
2023-03-05 19:51:21 +0000

    .

--- src/configs/token.ts
+++ src/configs/token.ts
@@ -1,6 +1,4 @@
 const botConfig = {
-    // "token": "MTA3OTEyMDczMDk4MzIzNTYxNQ.GpsmgH.m3GA1gItAFnl_x2eneIRhHOaRaBwg8KPqWmVQY",
-    "databaseToken": "mongodb+srv://tomg:tomasfrazao2007@cluster0.ffusx1w.mongodb.net"
 }
 
 export default botConfig
