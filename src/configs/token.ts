1f808205f20f9d7b471c0d5b0cfdbbbee66f4f25
TomG <81299743+TomG07@users.noreply.github.com>
2023-03-05 18:43:15 +0000

    Delete token.ts

--- src/configs/token.ts
+++
@@ -1,6 +0,0 @@
-const botConfig = {
-    "token": "MTA3OTEyMDczMDk4MzIzNTYxNQ.GdM5jN._-PRqD6P5cVde_WXdDQCddXvmWse7GjHS7yVLY",
-    "databaseToken": "mongodb+srv://tomg:tomasfrazao2007@cluster0.ffusx1w.mongodb.net"
-}
-
-export default botConfig
